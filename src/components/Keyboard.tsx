e5ac3a6f640fb1049df126d8bebed9e978e1dd6d
dmytro <dimitritkach@gmail.com>
2022-11-24 15:50:59 +0100

    Keyboard component styles

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,7 +1,48 @@
 import { FC } from 'react'
+import styles from './Keyboard.module.css'
+const KEYS = [
+  'q',
+  'w',
+  'e',
+  'r',
+  't',
+  'y',
+  'u',
+  'i',
+  'o',
+  'p',
+  'a',
+  's',
+  'd',
+  'f',
+  'g',
+  'h',
+  'j',
+  'k',
+  'l',
+  'z',
+  'x',
+  'c',
+  'v',
+  'b',
+  'n',
+  'm',
+]
 
 const Keyboard: FC = () => {
-  return <div>Keyboard</div>
+  return (
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: 'repeat(auto-fit, minmax(60px, 1fr))',
+        gap: '.5rem',
+      }}
+    >
+      {KEYS.map(key => (
+        <button key={key} className={styles.btn}>{key}</button>
+      ))}
+    </div>
+  )
 }
 
 export default Keyboard
